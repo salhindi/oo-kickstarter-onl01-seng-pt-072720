81d210e9b4cd0f31c672e3be65592218ddeea51a
Sara A <calisaraalhindi@gmail.com>
2020-09-02 00:21:11 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,6 +10,6 @@ class Project
   def add_backer(backer)
     @backers << backer
     #also adds project to the backers backed_projects array
-    backer.backed_projects
+    backer.backed_projects()
   end
 end
